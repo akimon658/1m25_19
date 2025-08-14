4b24c9aa94fa73c97165076a2bcc24e05b97fb31
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-08-14 20:39:34 +0900

    Refactor graph generation to use direct types for edges and graphs

--- crates/graph/src/lib.rs
+++ crates/graph/src/lib.rs
@@ -1,3 +1,4 @@
+use model::graph::{Edge, Graph};
 use rand::SeedableRng;
 use rand::seq::{IndexedRandom, SliceRandom};
 
@@ -6,11 +7,7 @@ pub struct GraphService {
 }
 
 impl GraphService {
-    pub async fn generate_graph(
-        &self,
-        num_nodes: u8,
-        num_edges: u8,
-    ) -> anyhow::Result<model::graph::Graph> {
+    pub async fn generate_graph(&self, num_nodes: u8, num_edges: u8) -> anyhow::Result<Graph> {
         if num_edges < num_nodes {
             return Err(anyhow::anyhow!(
                 "the number of edges must be at least equal to the number of nodes."
@@ -29,7 +26,7 @@ impl GraphService {
         // Randomize the answer
         hamiltonian_cycle.shuffle(&mut rng);
 
-        let mut edges: Vec<model::graph::Edge> = Vec::with_capacity(num_edges as usize);
+        let mut edges = Vec::<Edge>::with_capacity(num_edges as usize);
         let mut edge_set: std::collections::HashSet<(u8, u8)> = std::collections::HashSet::new();
 
         // Create a Hamiltonian cycle
@@ -37,19 +34,18 @@ impl GraphService {
             let source = hamiltonian_cycle[i as usize];
             let target = hamiltonian_cycle[((i + 1) % num_nodes) as usize];
 
-            edges.push(model::graph::Edge { source, target });
+            edges.push(Edge { source, target });
             edge_set.insert((source.min(target), source.max(target)));
         }
 
         // Add additional edges
-        let mut possible_edges = Vec::<model::graph::Edge>::with_capacity(
-            (num_nodes * (num_nodes - 1) / 2 - num_edges) as usize,
-        );
+        let mut possible_edges =
+            Vec::<Edge>::with_capacity((num_nodes * (num_nodes - 1) / 2 - num_edges) as usize);
 
         for i in 0..num_nodes {
             for j in (i + 1)..num_nodes {
                 if !edge_set.contains(&(i, j)) {
-                    possible_edges.push(model::graph::Edge {
+                    possible_edges.push(Edge {
                         source: i,
                         target: j,
                     });
@@ -60,13 +56,13 @@ impl GraphService {
         let sampled_edges = possible_edges
             .choose_multiple(&mut rng, (num_edges - num_nodes) as usize)
             .cloned()
-            .collect::<Vec<model::graph::Edge>>();
+            .collect::<Vec<Edge>>();
 
         edges.extend(sampled_edges);
         // Shuffle the edges to ensure randomness
         edges.shuffle(&mut rng);
 
-        let mut graph = model::graph::Graph {
+        let mut graph = Graph {
             id: 0, // ID will be returned by the repository
             num_nodes,
             edges,
