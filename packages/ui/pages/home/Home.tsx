44ec6fc2ca60b84405ed03451008dcbba33f79db
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-08-29 11:07:21 +0900

    Add graph generation functionality to Home component

--- packages/ui/pages/home/Home.tsx
+++ packages/ui/pages/home/Home.tsx
@@ -1,12 +1,23 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { Link } from "react-router"
-import { commands } from "../../api/bindings.gen.ts"
+import { commands, type GraphMetadata } from "../../api/bindings.gen.ts"
 import { keyGetGraphs } from "../../api/query_keys.ts"
+import { Button } from "../../components/Button.tsx"
 
 export const Home = () => {
   const { data } = useQuery({
     queryKey: keyGetGraphs,
     queryFn: commands.getGraphs,
+  })
+  const queryClient = useQueryClient()
+  const { mutate: generate_graph } = useMutation({
+    mutationFn: commands.generateGraph,
+    onSuccess: (data) => {
+      queryClient.setQueryData<GraphMetadata[]>(
+        keyGetGraphs,
+        (old) => [...(old || []), data],
+      )
+    },
   })
 
   return (
@@ -16,6 +27,7 @@ export const Home = () => {
           {graph.id}
         </Link>
       ))}
+      <Button onClick={() => generate_graph()}>生成</Button>
     </>
   )
 }
