e7f085a406707dd95fedaf0d78549ebab5717069
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-09-07 17:46:57 +0900

    アイコン・音声素材を追加 (#30)
    * クリア後の音声素材を追加
    
    * アイコンをtauri cliで生成
    
    * v0.1.10

--- packages/ui/pages/play/lib/dialogs.ts
+++ packages/ui/pages/play/lib/dialogs.ts
@@ -1,10 +1,14 @@
+import audio010 from "../../../assets/010.wav"
+import audio011 from "../../../assets/011.wav"
+import audio012 from "../../../assets/012.wav"
+import audio013 from "../../../assets/013.wav"
+import audio014 from "../../../assets/014.wav"
+import audio015 from "../../../assets/015.wav"
+import audio016 from "../../../assets/016.wav"
+import audio017 from "../../../assets/017.wav"
 import image4 from "../../../assets/yumeri4.png"
 import image6 from "../../../assets/yumeri6.png"
 import image7 from "../../../assets/yumeri7.png"
-// TODO: 実際の音声ファイルをインポートしてください
-// import clearPerfectAudio from "../../../assets/clear_perfect.wav"
-// import clearNormalAudio from "../../../assets/clear_normal.wav"
-// import stuckAudio1 from "../../../assets/stuck1.wav"
 
 export type Dialog = {
   text: string
@@ -15,20 +19,14 @@ export type Dialog = {
 export const clearDialogs = {
   perfect: [
     {
-      text:
-        "ゆめり「パーフェクトです！すごい！記憶の繋がりがはっきり見えました！」",
+      text: "ゆめり「完璧です！すごい！」",
       image: image7,
-      audioUrl: "", // clearPerfectAudio
+      audioUrl: audio010,
     },
     {
-      text: "ゆめり「完璧な記憶の道筋…！お見事です！」",
+      text: "ゆめり「お見事です！流石ですね」",
       image: image7,
-      audioUrl: "",
-    },
-    {
-      text: "ゆめり「パーフェクト！これぞ理想的な解法ですね！」",
-      image: image7,
-      audioUrl: "",
+      audioUrl: audio011,
     },
   ],
   normal: {
@@ -37,35 +35,29 @@ export const clearDialogs = {
         text:
           "ゆめり「クリアおめでとうございます！とってもスムーズでしたね！」",
         image: image4,
-        audioUrl: "", // clearNormalAudio
+        audioUrl: audio012,
       },
       {
-        text: "ゆめり「すごい速さ！記憶を辿る才能があるのかもしれませんね！」",
+        text: "ゆめり「すごい速さ！天才ですね」",
         image: image4,
-        audioUrl: "",
-      },
-      {
-        text: "ゆめり「お見事！時間内にクリアされました！」",
-        image: image4,
-        audioUrl: "",
+        audioUrl: audio013,
       },
     ],
     slow: [
       {
         text: "ゆめり「クリアです！一歩一歩、着実に記憶が繋がりましたね」",
         image: image4,
-        audioUrl: "",
+        audioUrl: audio014,
       },
       {
-        text:
-          "ゆめり「お疲れ様です！諦めずに繋げてくれてありがとうございます」",
+        text: "ゆめり「お疲れ様です！」",
         image: image4,
-        audioUrl: "",
+        audioUrl: audio015,
       },
       {
         text: "ゆめり「ゆっくりでも大丈夫！じっくり考えることも大切ですよ」",
         image: image4,
-        audioUrl: "",
+        audioUrl: audio016,
       },
     ],
   },
@@ -73,8 +65,8 @@ export const clearDialogs = {
 
 export const stuckDialogs: Dialog[] = [
   {
-    text: "ゆめり「うーん、行き詰まりみたいですね…」",
+    text: "ゆめり「うーん、行き詰まっちゃったみたいですね」",
     image: image6,
-    audioUrl: "", // stuckAudio1
+    audioUrl: audio017,
   },
 ]
